1200e1c5a1d992dfbe9e6cde679e10832a775d8a
Markus Merkle <markus.merkle277@gmail.com>
2019-04-30 19:45:02 +0200

    Output Text aus Property

--- cm-ui/source/main.cpp
+++ cm-ui/source/main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <controllers/master-controller.h>
+#include <QQmlContext>
 
 int main(int argc, char *argv[])
 {
@@ -7,14 +9,17 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/views/MasterView.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
+
+    qmlRegisterType<cm::controllers::MasterController>("CM", 1, 0, "MasterController");
+    cm::controllers::MasterController masterController;
+
+    QQmlApplicationEngine engine;           
+    engine.rootContext()->setContextProperty("masterController", &masterController);
     engine.load(url);
+
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }
